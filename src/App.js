3a8932e9469279db9dfb47532d695180e330a72d
Alex Kayser <akctba@gmail.com>
2020-05-14 12:11:21 -0700

    try

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import M from 'materialize-css';
 import { Navbar, NavItem, Row, Col, Icon } from 'react-materialize';
 import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import HeroCard from './components/HeroCard';
 import Result from './components/Result';
+import { connect, useDispatch } from "react-redux";
 
-function App() {
+function App({heroes, loadHeroes}) {
+
+  const [heroA, setHeroA] = useState();
+  const [heroB, setHeroB] = useState();
+  const [votes, setVotes] = useState();
 
   useEffect(()=> {
     M.AutoInit();
+
   }, []);
+
+  const start = () => {
+    //Pick two random heroes
+
+    let list = heroes.data.results;
+    let randomA = Math.floor(Math.random() * (list.length+1));
+    let randomB = randomA;
+    while (randomB !== randomA) {
+      randomB = Math.floor(Math.random() * (list.length+1));
+    }
+
+    setHeroA(list[randomA]);
+    setHeroB(list[randomB]);
+
+  }
 
   return (<>
     <Navbar
@@ -38,14 +59,33 @@ function App() {
       </NavItem>
     </Navbar>
       <Row><Col s={12}><h3>Between these two heroes, which one do you prefer?</h3></Col></Row>
+      {(!heroes?
+      <Row><Col>Loading...</Col></Row>
+      :(!heroA?
+        <Row><Col><button onClick={loadHeroes}>Start</button></Col></Row>
+        :
       <Row>
-        <Col l={4} m={6} s={6}><HeroCard /> </Col>
-        <Col l={4} m={6} s={6}><HeroCard /></Col>
+        <Col l={4} m={6} s={6}><HeroCard hero={heroA} /></Col>
+        <Col l={4} m={6} s={6}><HeroCard hero={heroB} /></Col>
         <Col l={4} m={12} s={12}><Result /></Col>
-      </Row>
+      </Row>)
+      )
+      }
       <p>Data provided by Marvel. © 2014 Marvel</p>
     </>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    heroes : state.heroes.heroes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadHeroes: dispatch({type: 'LOAD_HEROES'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Card, CardTitle, Icon, Button } from 'react-materialize';
 
-const HeroCard = () => {
+const HeroCard = (props) => {
+
+    const { hero } = props;
+
     return (
         <Card
             actions={[
-                <Button key="1" onClick={()=>{alert('yes')}}>Choose</Button>,
+                <Button key="1" onClick={()=>{alert(hero.id)}}>Choose</Button>,
                 <Button key="2" onClick={()=>{alert('info')}}>+Info</Button>
             ]}
             closeIcon={<Icon>close</Icon>}
-            header={<CardTitle image="https://materializecss.com/images/sample-1.jpg"><span className="heroname">Hero name</span></CardTitle>}
+        header={<CardTitle image="https://materializecss.com/images/sample-1.jpg"><span className="heroname">{hero.name}</span></CardTitle>}
             revealIcon={<Icon>more_vert</Icon>}
             >
             Hero story.
